a4626d896ab370b503eb7ddf0336c9642a977986
Owen Davies <tod11@doc.ic.ac.uk>
2014-02-25 19:08:12 +0000

    Removed everything except TODO. I could've added settings, but I want to make some other changes probably. Also added a gitignore

--- todo.py
+++ todo.py
@@ -27,10 +27,7 @@ DEFAULT_SETTINGS = {
     'result_title': 'TODO Results',
 
     'core_patterns': {
-        'TODO': r'TODO[\s]*?:+(?P<todo>.*)$',
-        'NOTE': r'NOTE[\s]*?:+(?P<note>.*)$',
-        'FIXME': r'FIX ?ME[\s]*?:+(?P<fixme>.*)$',
-        'CHANGED': r'CHANGED[\s]*?:+(?P<changed>.*)$'
+        'TODO': r'TODO[\s]*?:+(?P<todo>.*)$'
     },
 
     'patterns': {}
@@ -110,7 +107,7 @@ class ThreadProgress(object):
 
 
 class TodoExtractor(object):
-    def __init__(self, settings, filepaths, dirpaths, ignored_dirs, ignored_file_patterns, 
+    def __init__(self, settings, filepaths, dirpaths, ignored_dirs, ignored_file_patterns,
                  file_counter):
         self.filepaths = filepaths
         self.dirpaths = dirpaths
@@ -208,7 +205,7 @@ class TodoRenderer(object):
 
     @property
     def view(self):
-        existing_results = [v for v in self.window.views() 
+        existing_results = [v for v in self.window.views()
                             if v.name() == self.view_name and v.is_scratch()]
         if existing_results:
             v = existing_results[0]
@@ -220,9 +217,9 @@ class TodoRenderer(object):
         return v
 
     def format(self, messages):
-        """Yield lines for rendering into results view. Includes headers and 
+        """Yield lines for rendering into results view. Includes headers and
         blank lines.
-        Lines are returned in the form (type, content, [data]) where type is either 
+        Lines are returned in the form (type, content, [data]) where type is either
         'header', 'whitespace' or 'result'
         """
         key_func = lambda m: m['match'].type
@@ -326,7 +323,7 @@ class TodoCommand(sublime_plugin.TextCommand):
     def search_paths(self, window, open_files_only=False):
         """Return (filepaths, dirpaths)"""
         return (
-            [view.file_name() for view in window.views() if view.file_name()], 
+            [view.file_name() for view in window.views() if view.file_name()],
             window.folders() if not open_files_only else []
         )
 
@@ -350,7 +347,7 @@ class TodoCommand(sublime_plugin.TextCommand):
         exclude_file_patterns = [fnmatch.translate(patt) for patt in exclude_file_patterns]
 
         file_counter = FileScanCounter()
-        extractor = TodoExtractor(settings, filepaths, dirpaths, ignored_dirs, 
+        extractor = TodoExtractor(settings, filepaths, dirpaths, ignored_dirs,
                                   exclude_file_patterns, file_counter)
         renderer = TodoRenderer(settings, window, file_counter)
 
@@ -406,7 +403,7 @@ class GotoComment(sublime_plugin.TextCommand):
         selection = int(self.view.settings().get('selected_result', -1))
         ## Get the region
         selected_region = self.view.get_regions('results')[selection]
-        ## Convert region to key used in result_regions (this is tedious, but 
+        ## Convert region to key used in result_regions (this is tedious, but
         ##    there is no other way to store regions with associated data)
         data = self.view.settings().get('result_regions')['{0},{1}'.format(selected_region.a, selected_region.b)]
         self.log.debug(u'Goto comment at {filepath}:{linenum}'.format(**data))
